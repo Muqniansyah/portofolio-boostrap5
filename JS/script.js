f44f453f1a1177c713ef98c8925ac1e47e252c33
muqniansyah <muqniansyah@gmail.com>
2023-02-07 15:24:50 +0700

    menambahkan fitur contact form

--- JS/script.js
+++ JS/script.js
@@ -63,3 +63,30 @@
     });
   });
 })();
+
+// Contact Form
+const scriptURL = "https://script.google.com/macros/s/AKfycbxsvCuPUgybnicDTcL9SRZJQODX60LCzE34C2UZw0Pu9FrouQyG1Zs2_yybf8RQSu3Q/exec";
+const form = document.forms["muqni-contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset formnya
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
